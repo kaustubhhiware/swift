1900e464c44e771353a776c43e73d483e532ccd5
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2019-03-31 23:29:00 +0530

    Init code for node

---
+++ node/__main__.py
@@ -0,0 +1,17 @@
+"""
+Each node starts with broadcasting on a port.
+Every node responds back with the requesting IP whose download is going on, if any
+"""
+
+import node
+
+NODE_SEND_PORT = 8192
+NODE_RECV_PORT = 8193
+JOIN_TIMEOUT = 30 # seconds
+
+def beacon():
+
+
+if __name__ == '__main__':
+	# First send a beacon signalling requesting IP
+	beacon()
