3aa822c8a683f62c3cab42d76cde52921999a681
Pranay Pratyush <pranay.pratyush@gmail.com>
2019-04-07 20:09:51 +0530

    Discovery

---
+++ discovery.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+
+s = socket.socket()
+s.bind(("localhost", 4444))
+s.listen(20)
+f = open('discovered.txt','+')
+while True:
+	sc, address = s.accept()
+	print(address)
+	data = f.readlines()
+	for line in data:
+		if line == address:
+			data.remove(line)
+	data.append(address)
+	f.seek(0)
+	f.writelines(data)
+	s.sendall(f)
+	sc.close()
+s.close()
+f.close()
+
+# import socket
+# import io
+#
+# vfile = io.StringIO()
+# s = socket.socket()
+# s.connect('discoveryip', 4444)
+# d = s.recv(65565)
+# print(d)
+# while d:
+# 	vfile.write(d)
+# 	d = s.recv(65565)
+# s.close()
+# vfile.seek(0)
+#
+# # data = vfile.read
+# #data should be a list of strings of addresses
